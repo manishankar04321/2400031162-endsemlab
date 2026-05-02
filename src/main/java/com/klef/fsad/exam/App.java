a8ea09cfa0f38499c0210d06ca3f614c293f2eea
manishankar04321 <manishankar04321@gmail.com>
2026-05-02 18:45:46 +0530

    Initial commit - Hibernate HQL Booking Project

---
+++ src/main/java/com/klef/fsad/exam/App.java
@@ -0,0 +1,7 @@
+package com.klef.fsad.exam;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/main/java/com/klef/fsad/exam/Booking.java
@@ -0,0 +1,36 @@
+package com.klef.fsad.exam;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "booking")
+public class Booking 
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String date;
+    private String status;
+
+    public Booking() {}
+
+    public Booking(String name, String date, String status) {
+        this.name = name;
+        this.date = date;
+        this.status = status;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getDate() { return date; }
+    public void setDate(String date) { this.date = date; }
+
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
+}
